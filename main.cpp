8f2bb7eafe63cf6f43a92cfd5431fc50b7a394a3
vvcaw <eric.elflein@googlemail.com>
2023-05-06 23:23:06 +0200

    Hello World in nvim

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
-#include <iostream>
 #include "rpc/client.h"
+#include "rpc/rpc_error.h"
+
+#include <exception>
+#include <iostream>
 
 int main() {
-    // Creating a client that connects to the localhost on port 8080
-    rpc::client client("127.0.0.1", 8080);
+  // Creating a client that connects to the localhost on port 8080
+  rpc::client client("127.0.0.1", 6666);
 
-    // Calling a function with paramters and converting the result to int
-    auto result = client.call("add", 2, 3).as<int>();
-    std::cout << "The result is: " << result << std::endl;
-    return 0;
+  std::cout << "Got here lel" << std::endl;
+
+  // Calling a function with paramters and converting the result to int
+  try {
+    client.call("nvim_command", "echo 'Hello, World!'");
+  } catch (rpc::rpc_error &e) {
+    std::cout << std::endl << e.what() << std::endl;
+    std::cout << "in function " << e.get_function_name() << ": ";
+
+    using err_t = std::tuple<int, std::string>;
+    auto err = e.get_error().as<err_t>();
+    std::cout << "[error " << std::get<0>(err) << "]: " << std::get<1>(err)
+              << std::endl;
+  }
+
+  return 0;
 }
